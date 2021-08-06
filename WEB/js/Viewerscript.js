24f1ef9168485e1324178a3fca430a3e7e1cc1b1
Rpa1Labs <Rpa1Labs@github.com>
2021-08-06 19:36:16 +0200

    Rotation de la matrice 3D pour être plus 'User friendly'

--- WEB/js/Viewerscript.js
+++ WEB/js/Viewerscript.js
@@ -7,9 +7,13 @@
 * http://www.instructables.com/id/Led-Cube-8x8x8/ by Christian Moen and
 * Ståle Kristoffersen.
 * http://www.kevindarrah.com/?cat=99 by Kevin Darrah
+* Optimisé par
+* https://github.com/Robotek-Orleans/LED-Cube/tree/main/WEB by Robotek Orléans
 */
 
 if (!Detector.webgl) Detector.addGetWebGLMessage();
+
+//LED draw parameters 
 
 let container, scene, camera, renderer, controls, stats;
 const RES = 8;
@@ -17,19 +21,29 @@ const distanceLED = 40;
 let materialGlow, materialPoint, materialOff, wireFrame;
 let paused = true;
 
-var colorOn = 1;
+const offset_x = -distanceLED * (RES - 1) / 2, offset_y = -distanceLED * (RES - 1) / 2, offset_z = -distanceLED * (RES - 1) / 2;
 
-function initGL() {
+//Wireframe parameters
+const pw = distanceLED/2 // Plan Width
+const vx = [ offset_x - distanceLED/2, -offset_x + distanceLED/2 ] // xmin,xmax
+const vy = [ offset_y - distanceLED/2, -offset_y + distanceLED/2 ] // ymin,ymax
+const vz = [ offset_z - distanceLED/2, -offset_z + distanceLED/2 ] // zmin,zmax
+
+async function initGL() {
+
+    THREE.Object3D.DefaultUp.set(0.0, 0.0, -1.0);
 
     scene = new THREE.Scene();
     container = document.getElementById('container');
+
+    
 
     // camera
     var SCREEN_WIDTH = container.offsetWidth, SCREEN_HEIGHT = container.offsetHeight;
     var VIEW_ANGLE = 45, ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT, NEAR = 0.1, FAR = 20000;
     camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
     scene.add(camera);
-    camera.position.set(-400, 500, 1000);
+    camera.position.set(-400, 500, -1000);
     camera.lookAt(scene.position);
 
     // renderer
@@ -44,22 +58,19 @@ function initGL() {
     controls = new THREE.OrbitControls(camera, renderer.domElement);
     controls.maxPolarAngle = Math.PI / 2;
 
-
-    var d = distanceLED;
-    var offset_x = -d * (RES - 1) / 2, offset_y = -d * (RES - 1) / 2 + 30, offset_z = -150;
     const s2 = 15;
 
-    var textureGlow = THREE.ImageUtils.loadTexture('images/textures/ledon.svg');
-    var textureOff = THREE.ImageUtils.loadTexture('images/textures/ledoff.svg');
+    var textureGlow = await THREE.ImageUtils.loadTexture('images/textures/ledon.svg');
+    var textureOff = await THREE.ImageUtils.loadTexture('images/textures/ledoff.svg');
 
     materialGlow = new THREE.SpriteMaterial({ map: textureGlow, color: 0x0, transparent: true });
     materialOff = new THREE.SpriteMaterial({ map: textureOff, color: 0xffffff, transparent: true, opacity: 0.5 });
 
-    for (var z = 0; z < RES; z++) {
-        for (var y = 0; y < RES; y++) {
-            for (var x = 0; x < RES; x++) {
-                var sprite = new THREE.Sprite(materialOff);
-                sprite.position.set(offset_x + d * x, offset_y + d * y, offset_z + d * z);
+    for (let z = 0; z < RES; z++) {
+        for (let y = 0; y < RES; y++) {
+            for (let x = 0; x < RES; x++) {
+                const sprite = new THREE.Sprite(materialOff);
+                sprite.position.set( offset_x + distanceLED * x , offset_y + distanceLED * y, offset_z + distanceLED * z);
                 sprite.scale.set(s2, s2, s2);
                 scene.add(sprite);
             }
@@ -77,45 +88,40 @@ function displaySelectedPlan(plan, nb) {
     var geometry = new THREE.Geometry();
 
     plan = plan.toUpperCase()
-    nb = (nb - 1) * 40
-    
-    const pw = 20 // Plan Width
+    nb = (nb - 1) * distanceLED + distanceLED/4
     
     scene.remove(wireFrame)
 
-    const x = [ -150, 150] // xmin,xmax
-    const y = [ -120, 180] // ymin,ymax
-    const z = [ -160, 140] // zmin,zmax
     const fvp = [0,0,1,1,0] // First Vertex Patern
     const lvp = [0,1,1,0,0] // Last Vertex Patern
 
     switch(plan){
         case "X":
             for(let i=0; i < fvp.length; i++){
-                geometry.vertices.push(new THREE.Vector3( x[0]+nb, y[fvp[i]], z[lvp[i]] ))
+                geometry.vertices.push(new THREE.Vector3( vx[0]+nb, vy[fvp[i]], vz[lvp[i]] ))
             }
             for(let i=0; i < fvp.length; i++){
-                geometry.vertices.push(new THREE.Vector3( x[0]+nb+pw, y[fvp[i]], z[lvp[i]] ))
+                geometry.vertices.push(new THREE.Vector3( vx[0]+nb+pw, vy[fvp[i]], vz[lvp[i]] ))
             }
-            geometry.vertices.push(new THREE.Vector3(x[0]+nb, y[1], z[0]))
+            geometry.vertices.push(new THREE.Vector3(vx[0]+nb, vy[1], vz[0]))
             break;
         case "Y":
             for(let i=0; i < fvp.length; i++){
-                geometry.vertices.push(new THREE.Vector3( x[fvp[i]], y[0]+nb, z[lvp[i]] ))
+                geometry.vertices.push(new THREE.Vector3( vx[fvp[i]], vy[0]+nb, vz[lvp[i]] ))
             }
             for(let i=0; i < fvp.length; i++){
-                geometry.vertices.push(new THREE.Vector3( x[fvp[i]], y[0]+nb+pw, z[lvp[i]] ))
+                geometry.vertices.push(new THREE.Vector3( vx[fvp[i]], vy[0]+nb+pw, vz[lvp[i]] ))
             }
-            geometry.vertices.push(new THREE.Vector3(x[1], y[0]+nb, z[0]))
+            geometry.vertices.push(new THREE.Vector3(vx[1], vy[0]+nb, vz[0]))
             break;
         case "Z":
             for(let i=0; i < fvp.length; i++){
-                geometry.vertices.push(new THREE.Vector3( x[fvp[i]], y[lvp[i]], z[0]+nb ))
+                geometry.vertices.push(new THREE.Vector3( vx[fvp[i]], vy[lvp[i]], vz[0]+nb ))
             }
             for(let i=0; i < fvp.length; i++){
-                geometry.vertices.push(new THREE.Vector3( x[fvp[i]], y[lvp[i]], z[0]+nb+pw ))
+                geometry.vertices.push(new THREE.Vector3( vx[fvp[i]], vy[lvp[i]], vz[0]+nb+pw ))
             }
-            geometry.vertices.push(new THREE.Vector3(x[1], y[0], z[0]+nb))
+            geometry.vertices.push(new THREE.Vector3(vx[1], vy[0], vz[0]+nb))
             break;
     }
 
@@ -153,12 +159,12 @@ function cube_get_color(x, y, z) {
         return -1;
     }
 
-    var i = 1 + (x + y * RES + z * RES * RES) * 2;
-    return scene.children[i + 1].material.color.getHex();
+    var i = 1 + x + y * RES + z * RES * RES;
+    return scene.children[i].material.color.getHex();
 }
 
 function get_scene_offset(x, y, z) {
-    return 1 + (x + y * RES + z * RES * RES);
+    return 1 + x + y * RES + z * RES * RES;
 }
 
 function cube_set_color(x, y, z, color) {
@@ -170,7 +176,7 @@ function cube_set_color(x, y, z, color) {
         return;
     }
 
-    var i = get_scene_offset(x, y, z);
+    const i = get_scene_offset(x, y, z);
 
     if (color == 0x0) {
         // turn off the LED
