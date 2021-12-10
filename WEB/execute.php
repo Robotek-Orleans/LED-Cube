7d6c0d26c835279a7d1e7d1813fb2329f47d60c2
Rpa1Labs <Rpa1Labs@github.com>
2021-12-10 21:52:54 +0100

    Ajout php exécution

--- WEB/execute.php
+++ WEB/execute.php
@@ -1,5 +1,16 @@
 <?php
 
-	exec('sudo /var/www/html/LED-Cube/cpp/execute');
+    http_response_code(200);
+
+    exec('sudo pkill execLEDCube');
+
+    if(isset($_GET['f'])){
+       exec('sudo /var/www/html/LED-Cube/cpp/execLEDCube '.$_GET['f']. " > /dev/null &");
+       echo "OK";
+    }else{
+        http_response_code(400);
+        die('bad request');
+    }
+    
 
 ?>
