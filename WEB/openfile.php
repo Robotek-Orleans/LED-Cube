3d4b278153a696ceda6067481d4dc3b0348dfde6
Rpa1Labs <Rpa1Labs@github.com>
2021-12-12 21:17:14 +0100

    Implémentation ouverture fichier

--- WEB/openfile.php
+++ WEB/openfile.php
@@ -7,32 +7,32 @@ $path = "./animations/";
 
 $dataArray = array();
 
- if(isset($_GET['f'])){
-    $file = fopen($path.$_GET['f'], "r") or die("Unable to open file!");
+if (isset($_GET['f'])) {
+    $file = fopen($path . $_GET['f'], "r") or die("Unable to open file!");
     $frames = intval(fgets($file));
     $time = intval(fgets($file));
 
     if ($file) {
-        for ($i=0;$i<$frames;$i++) {
+        for ($i = 0; $i < $frames; $i++) {
             $dataArray[$i] = array();
-            for ($j=0;$j<8;$j++) {
+            for ($j = 0; $j < 8; $j++) {
                 $dataArray[$i][$j] = array();
-                for ($k=0;$k<8;$k++) {
+                for ($k = 0; $k < 8; $k++) {
                     $dataArray[$i][$j][$k] = array();
-                    for ($l=0;$l<8;$l++) {
+                    for ($l = 0; $l < 8; $l++) {
                         $red    = dechex(intval(fgets($file)));
-                        if(strlen($red)<2){
-                            $red = '0'.$red;
+                        if (strlen($red) < 2) {
+                            $red = '0' . $red;
                         }
                         $green  = dechex(intval(fgets($file)));
-                        if(strlen($green)<2){
-                            $green = '0'.$green;
+                        if (strlen($green) < 2) {
+                            $green = '0' . $green;
                         }
                         $blue   = dechex(intval(fgets($file)));
-                        if(strlen($blue)<2){
-                            $blue = '0'.$blue;
+                        if (strlen($blue) < 2) {
+                            $blue = '0' . $blue;
                         }
-                        $dataArray[$i][$j][$k][$l] = "#".$red.$green.$blue;
+                        $dataArray[$i][$j][$k][$l] = "#" . $red . $green . $blue;
                     }
                 }
             }
@@ -40,12 +40,12 @@ $dataArray = array();
         }
     } else {
         // error opening the file.
-    } 
+    }
     fclose($file);
- }
+}
 
 
- 
+
 
 ?>
 
@@ -60,7 +60,7 @@ $dataArray = array();
     <script src="js/OrbitControls.js"></script>
     <script src="js/Viewerscript.js"></script>
     <script src="js/Main.js"></script>
-    <script src="js/Request.js"></script> 
+    <script src="js/Request.js"></script>
     <script>
         var selectedPlanDirection = "X"; // X , Y ou Z
         var selectedPlanNumber = 1; // entre 1 et 8
@@ -70,17 +70,17 @@ $dataArray = array();
         var copied2D = [];
         var copied3D = [];
         <?php
-            if(isset($_GET['f'])){
-                echo "var framecontent = ".json_encode($dataArray).";";
-            }else{
-                echo "var framecontent = [];";
-                echo "addframe(0);";
-            }
+        if (isset($_GET['f'])) {
+            echo "var framecontent = " . json_encode($dataArray) . ";";
+        } else {
+            echo "var framecontent = [];";
+            echo "addframe(0);";
+        }
         ?>
     </script>
     <style>
 
-</style>
+    </style>
 </head>
 
 <body onload="init()">
@@ -94,27 +94,27 @@ $dataArray = array();
         <div class="contentviewer morewith">
             <h1 class="title">Gestion du fichier</h1>
 
-            
 
 
-            
-<?php
-$gfg_folderpath = "animations/";
-// CHECKING WHETHER PATH IS A DIRECTORY OR NOT
-if (is_dir($gfg_folderpath)) {
-    // GETTING INTO DIRECTORY
-    $files = opendir($gfg_folderpath); {
-        // CHECKING FOR SMOOTH OPENING OF DIRECTORY
-        if ($files) {
-            //READING NAMES OF EACH ELEMENT INSIDE THE DIRECTORY
-            $i=0;
-            while (($gfg_subfolder = readdir($files)) !== FALSE) {
-                // CHECKING FOR FILENAME ERRORS
-             if ($gfg_subfolder != '.' && $gfg_subfolder != '..') {
-                 $i = $i +1;
-                 if ($gfg_subfolder != ".htaccess") {
-                    echo "<br>  
-                    <button id='myBtn' onClick='getFileName()' class='$gfg_subfolder'>".$gfg_subfolder."</button>
+
+
+            <?php
+            $gfg_folderpath = "animations/";
+            // CHECKING WHETHER PATH IS A DIRECTORY OR NOT
+            if (is_dir($gfg_folderpath)) {
+                // GETTING INTO DIRECTORY
+                $files = opendir($gfg_folderpath); {
+                    // CHECKING FOR SMOOTH OPENING OF DIRECTORY
+                    if ($files) {
+                        //READING NAMES OF EACH ELEMENT INSIDE THE DIRECTORY
+                        $i = 0;
+                        while (($gfg_subfolder = readdir($files)) !== FALSE) {
+                            // CHECKING FOR FILENAME ERRORS
+                            if ($gfg_subfolder != '.' && $gfg_subfolder != '..') {
+                                $i = $i + 1;
+                                if ($gfg_subfolder != ".htaccess") {
+                                    echo "<br>  
+                    <button id='myBtn' onClick='getFileName()' class='$gfg_subfolder'>" . $gfg_subfolder . "</button>
                     <hr class='morewith'>
                     
                     --<br>
@@ -122,107 +122,107 @@ if (is_dir($gfg_folderpath)) {
           
 
                     ";
-                 }
-                    
-                     
-                $dirpath = "animations/" . $gfg_subfolder . "/";
-                    // GETTING INSIDE EACH SUBFOLDERS
-                    if (is_dir($dirpath)) {
-                        $file = opendir($dirpath); {
-                            if ($file) {
-                //READING NAMES OF EACH FILE INSIDE SUBFOLDERS
-               while (($gfg_filename = readdir($file)) !== FALSE) {
-                if ($gfg_filename != '.' && $gfg_filename != '..') {
-                        echo $gfg_filename . "<br>";
-                           }
-                         }
-                      }
-                   }
-               }
-                    echo "<br>";
+                                }
+
+
+                                $dirpath = "animations/" . $gfg_subfolder . "/";
+                                // GETTING INSIDE EACH SUBFOLDERS
+                                if (is_dir($dirpath)) {
+                                    $file = opendir($dirpath); {
+                                        if ($file) {
+                                            //READING NAMES OF EACH FILE INSIDE SUBFOLDERS
+                                            while (($gfg_filename = readdir($file)) !== FALSE) {
+                                                if ($gfg_filename != '.' && $gfg_filename != '..') {
+                                                    echo $gfg_filename . "<br>";
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                                echo "<br>";
+                            }
+                        }
+                    }
                 }
             }
-        }
-    }
-}
-?>
+            ?>
 
         </div>
 
-        
+
+    </div>
+
+
+    <!-- The Modal -->
+    <div id="myModal" class="modal contentviewer">
+
+        <!-- Modal content -->
+        <div class="modal-content">
+            <span class="close">&times;</span>
+
+
+
+            <div class="contentviewer">
+
+                <div id="container"></div>
+            </div>
+            <div>
+                <a href="#" class="link">Vusialiser</button>
+                    <a id="edit" href="" class="link">Editer</a>
+            </div>
+
+
         </div>
 
-
-                        <!-- The Modal -->
-                        <div id="myModal" class="modal contentviewer">
-
-                            <!-- Modal content -->
-                                <div class="modal-content">
-                                <span class="close">&times;</span>
+    </div>
 
 
 
-                                    <div class="contentviewer">
 
-                                        <div id="container"></div>
-                                    </div>
-                                <div>
-                                    <a href="#" class="link">Vusialiser</button>
-                                    <a id="edit" href="" class="link">Editer</a>
-                        </div>
-
-
-                        </div>
-
-                        </div>
-    
-
-        
-
-        </div>
+    </div>
     </div>
     <table id="contentnotifs"></table>
 
     <script>
-// Get the modal
-var modal = document.getElementById("myModal");
+        // Get the modal
+        var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+        // Get the button that opens the modal
+        var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-    
-  // var classContent = btn.getAttibute('class');
+        // When the user clicks the button, open the modal 
+        btn.onclick = function() {
+            modal.style.display = "block";
 
-   var x = this.getAttribute("class");  
+            // var classContent = btn.getAttibute('class');
 
-   var loc = "index.php?f="+x;
+            var x = this.getAttribute("class");
 
-
-   document.getElementById("edit").href = loc;
-
-   console.log(loc);
+            var loc = "index.php?f=" + x;
 
 
-}
+            document.getElementById("edit").href = loc;
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+            console.log(loc);
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-</script>
+
+        }
+
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+    </script>
 </body>
 
 
