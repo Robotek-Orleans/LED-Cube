060ac0b817ce462066be8a2f4e1a336161b91ac7
Rpa1Labs <Rpa1Labs@github.com>
2022-01-04 19:15:10 +0100

    correction faille sécurité

--- WEB/execute.php
+++ WEB/execute.php
@@ -11,7 +11,7 @@ redirect_user_if_is_not_log_in();
 
     exec('sudo pkill -SIGINT execLEDCube');
 
-    if(isset($_GET['f'])){
+    if(isset($_GET['f']) && isset($_SESSION['username'])){
        exec('sudo /var/www/html/LED-Cube/cpp/execLEDCube '.$_GET['f']. " > /dev/null &");
        echo "OK";
     }else{
